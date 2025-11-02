74b838ddd4cf84856bc8acc3202326351de2ab78
Heena <heenashaikh1811@gmail.com>
2025-11-02 13:08:07 +0530

    Requirement 3:
    Created AuthTokenProvider- Manag the Token for Differenert Roles/
    
    Roles: Hold all the User Roles of the Test checks the status code,
    response time and Schema of the UserDetails API
    
    CountAPITest: Functional API Test checks the status code response time
    and Schema of the Count API. Also does the token testing for negative
    
    MasterAPI: Functional API Test checks the status code, response time and Schema of the Master API does the token testing for negative scenarios

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -5,9 +5,13 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.lessThan;
 
 import java.io.IOException;
+import java.security.AuthProvider;
 
 import org.testng.annotations.Test;
 
+import static com.api.constant.Role.*;
+
+import static com.utils.AuthTokenPovider.*;
 
 import static com.utils.ConfigManager.*;
 
@@ -23,7 +27,7 @@ public class UserDetailsAPITest {
 		
 	//	ConfigManager configManager = new ConfigManager();
 		
-		Header authHeader = new Header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NjE3MTIyNDB9.jbwipZKHKfqtmno_nyzAbqS-HMJ5nLlB2A2sVr6Vh5k");
+		Header authHeader = new Header("Authorization", getToken(FD));
 				given()
 				.baseUri(getProperty("BASE_URI"))
 				.and()
---
+++ src/test/java/com/utils/AuthTokenPovider.java
@@ -0,0 +1,67 @@
+package com.utils;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.awt.Robot;
+
+import static  com.api.constant.Role.*;
+
+import com.api.constant.Role;
+import com.api.pojo.UserCredentials;
+
+import io.restassured.http.ContentType;
+
+public class AuthTokenPovider {
+	
+	private AuthTokenPovider() {
+		
+	}
+
+	//public static void main(String[] args) {
+		public static String getToken( Role role) {
+		//make the request for the login api and want to extract the token
+		//print it on the console!
+		
+		UserCredentials userCredentials = null;
+		//if(role.equalsIgnoreCase("FD"))
+			if(role==FD)
+		{
+			userCredentials=new UserCredentials("iamfd", "password");
+		}	
+		
+		else if(role==SUP)
+		{
+			userCredentials=new UserCredentials("iamsup", "password");
+		}	
+		else if(role==ENG)
+		{
+			userCredentials=new UserCredentials("iameng", "password");
+		}	
+		else if(role==QC)
+		{
+			userCredentials=new UserCredentials("iamqc", "password");
+		}	
+			
+	String token =	given()
+		.baseUri(ConfigManager.getProperty("BASE_URI"))
+		.contentType(ContentType.JSON)
+		//.body(new UserCredentials("iamfd", "password"))
+		.body(userCredentials)
+		.when()
+		.post("login")
+		.then()
+		.log().ifValidationFails()
+		.statusCode(200)
+		.body("message",equalTo("Success"))
+		.extract()
+		.body()
+		.jsonPath()
+		.getString("data.token");
+		
+//	System.out.println("------------------------------------");
+//	System.out.println(token);
+	return token;
+	}
+
+}
