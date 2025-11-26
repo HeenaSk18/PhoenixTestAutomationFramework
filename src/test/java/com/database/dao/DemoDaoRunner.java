791cff4e4caa6fcd98a9968a551f02315e35f754
Heena <heenashaikh1811@gmail.com>
2025-11-26 12:03:39 +0530

    Create CustomerDao and CustomerDBModel for Requirememt 13

---
+++ src/test/java/com/database/dao/DemoDaoRunner.java
@@ -0,0 +1,24 @@
+package com.database.dao;
+
+import java.sql.SQLException;
+
+import org.testng.Assert;
+
+import com.api.request.model.Customer;
+import com.database.model.CustomerDBModel;
+
+public class DemoDaoRunner {
+
+	public static void main(String[] args) throws SQLException {
+		CustomerDBModel customerDBData= CustomerDao.getCustomerInfo();
+		System.out.println(customerDBData);
+	System.out.println(customerDBData.getFirst_name());
+	System.out.println(customerDBData.getEmail_id());
+	System.out.println(customerDBData.getMobile_number());
+	Customer customer = new Customer("Jatin","Shharma","7045663552","","jatinvshharma@gmail.com", "");
+	Assert.assertEquals(customerDBData.getFirst_name(), customer.first_name());
+	
+	
+	}
+
+}
